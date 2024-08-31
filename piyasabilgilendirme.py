d8cf70d1c7cecffd05e7231055343e7bca6c6f56
Furkan B. <114612111+babazibidi@users.noreply.github.com>
2024-08-31 23:44:12 +0300

    Update piyasabilgilendirme.py

--- piyasabilgilendirme.py
+++ piyasabilgilendirme.py
@@ -1,3 +1,4 @@
+import re
 import yfinance as yf
 import requests
 import pandas as pd
@@ -7,17 +8,16 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import locale
 now_day = datetime.now()
+import seffaflik2 as sf
+import matplotlib.dates as mdates
 
-
-params = {
-    'startDate': '2024-07-24T00:00:00+03:00',
-    'endDate': '2024-08-25T23:59:59+03:00'
-}
+start_date = '2024-07-23'
+end_date = '2024-08-30'
 
 brent_symbol = "BZ=F"
 coal_symbol = "MTF=F"
 ttf_symbol = "TTF=F"
-henry_symbol = "HH=F"
+henry_symbol = "HP=F"
 
 brent_data = yf.Ticker(brent_symbol)
 brent_history = brent_data.history(period="1y")
@@ -43,6 +43,7 @@ plt.grid(True)
 plt.show()
 
 url = 'https://api.exchangerate-api.com/v4/latest/EUR'
+
 try:
     response = requests.get(url)
     data = response.json()
@@ -53,15 +54,15 @@ except requests.exceptions.RequestException as e:
 ttf_data = yf.Ticker(ttf_symbol)
 henry_hub_data = yf.Ticker(henry_symbol)
 ttf_history = ttf_data.history(period="1y")
-ttf_history['Close_new'] = ttf_history['Close']* euro_to_usd *10.55
+ttf_history['Close_new'] = ttf_history['Close']* euro_to_usd * 10.55
 henry_hub_history = henry_hub_data.history(period="1y")
-henry_hub_history['Close_new'] = henry_hub_history['Close']* euro_to_usd *10.55
+henry_hub_history['Close_new'] = henry_hub_history['Close']*35.71
 last_date_ttf = ttf_history.index[-1]
 last_price_ttf = ttf_history['Close'].iloc[-1] * euro_to_usd * 10.55
 last_date_henry = henry_hub_history.index[-1]
-last_price_henry = henry_hub_history['Close'].iloc[-1] * euro_to_usd * 10.55
+last_price_henry = henry_hub_history['Close'].iloc[-1] * 35.71
 plt.figure(figsize=(14,8))
-plt.plot(ttf_history.index, ttf_history['Close_new'], label='TFF Gaz ($/1000sm3)', color='gray')
+plt.plot(ttf_history.index, ttf_history['Close_new'], label='TTF Gaz ($/1000sm3)', color='gray')
 plt.scatter(last_date_ttf, last_price_ttf, color='gray')
 plt.text(last_date_ttf, last_price_ttf, f'{last_price_ttf:.2f}', color='gray', fontsize=12, ha='left', va='bottom')
 plt.plot(henry_hub_history.index, henry_hub_history['Close_new'], label='Henry Gaz ($/1000sm3)', color='blue')
@@ -74,40 +75,113 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-gerceklesen_url = 'https://seffaflik.epias.com.tr/transparency/service/consumption/real-time-consumption'
-gerceklesen_response = requests.get(gerceklesen_url, params=params)
-gerceklesen_df = pd.DataFrame.from_dict(gerceklesen_response.json()['body']['hourlyConsumptions'])
-gerceklesen_df['date']= pd.to_datetime(gerceklesen_df['date'])
-gerceklesen_gunluk = gerceklesen_df.resample('D', on='date').sum()/1000
+my_mail = 'm.furkan.baysal@hotmail.com'
+my_password = 'Zorlu.2024'
+def date_converter(date_string):
+    new_string = date_string+"T00:00:00+03:00"
+    return new_string
 
-ptf_url = 'https://seffaflik.epias.com.tr/transparency/service/market/mcp-smp'
-ptf_response = requests.get(ptf_url, params=params)
-ptf_df = pd.DataFrame.from_dict(ptf_response.json()['body']['mcpSmps'])
-ptf_df['date'] = pd.to_datetime(ptf_df['date'])
-columns_to_drop = ['smp', 'smpDirection', 'mcpState']
-ptf_df.drop(columns=columns_to_drop, inplace=True)
-ptf_gunluk = ptf_df.resample('D', on='date').mean()
+def epias_tgt(e_mail, psw):
+    url = f'https://giris.epias.com.tr/cas/v1/tickets?username={e_mail}&password={psw}'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/plain'
+    }
+    global response
+    response = requests.request('POST', url, headers=headers, timeout=30)
+    if response.status_code == 200:
+        return response.text
 
-fig, ax1 = plt.subplots(figsize= (20, 6))
+def epias_demand(start_date, end_date, tgt, e_mail, psw):
+    sd = date_converter(start_date)
+    ed = date_converter(end_date)
+    url = f'https://seffaflik.epias.com.tr/electricity-service/v1/consumption/data/realtime-consumption?username={e_mail}&password={psw}'
 
-color = 'tab:blue'
-ax1.set_xlabel('TARİH')
-ax1.set_ylabel('TÜKETİM(GWh)', color=color)
-ax1.plot(gerceklesen_gunluk.index, gerceklesen_gunluk, color=color, label='Consumption(MWh)')
-ax1.tick_params(axis='y', labelcolor=color)
+    payload = json.dumps({
+        'endDate': ed,
+        'startDate': sd,
+        'region': 'TR1'
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'TGT': tgt
+    }
 
-for i, v in gerceklesen_gunluk.iterrows():
-    ax1.text(i, v[0], f'{v[0]:.0f}', ha='center', va='bottom', color=color)
+    response = requests.request('POST', url, headers=headers, data=payload, timeout=30)
 
+    if response.status_code == 200:
+        df_consumption = pd.json_normalize(response.json()['items'])
+        df_consumption['date'] = pd.to_datetime(df_consumption['date'])
+        return df_consumption
+    else:
+        response.text
+
+
+def epias_mcp(start_date, end_date, tgt, e_mail, psw):
+    sd = date_converter(start_date)
+    ed = date_converter(end_date)
+    url = f'https://seffaflik.epias.com.tr/electricity-service/v1/markets/dam/data/mcp?username={e_mail}&password={psw}'
+
+    payload = json.dumps({
+        'endDate': ed,
+        'startDate': sd,
+        'region': 'TR1'
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'TGT': tgt
+    }
+
+    response = requests.request('POST', url, headers=headers, data=payload, timeout=30)
+
+    if response.status_code == 200:
+        df_ptf = pd.json_normalize(response.json()['items'])
+        df_ptf['date'] = pd.to_datetime(df_ptf['date'])
+        return df_ptf.resample('D', on='date').mean()
+    else:
+        response.text
+
+myTGT = sf.epias_tgt(my_mail, my_password)
+
+df_ptf = sf.epias_mcp(start_date, end_date, myTGT, my_mail, my_password)
+df_ptf.drop(['hour', 'priceUsd', 'priceEur'], axis=1, inplace=True)
+df_ptf = df_ptf.resample('D', on='date').mean()
+
+df_dmd = sf.epias_demand(start_date, end_date, myTGT, my_mail, my_password)
+df_dmd.drop(['time'], axis=1, inplace=True)
+df_dmd = df_dmd.resample('D', on='date').sum()
+
+df_merge = df_ptf.merge(df_dmd, on='date')
+
+fig, ax1 = plt.subplots(figsize=(16, 8))
+
+ax1.plot(df_dmd.index, df_dmd['consumption'] / 1000, 'g-', label='Tüketim (GWh)')
+ax1.set_xlabel('Tarih')
+ax1.set_ylabel('Tüketim (GWh)', color='red')
+ax1.set_ylim(0, 1200)
+ax1.tick_params(axis='y', labelcolor='red')
+
+# Create a second y-axis for df_ptf
 ax2 = ax1.twinx()
-color = 'tab:red'
-ax2.set_ylabel('PTF (TL/MWh)', color=color)
-ax2.plot(ptf_gunluk.index, ptf_gunluk, color=color, label='PTF (TL/MWh)')
-ax2.tick_params(axis='y', labelcolor=color)
+ax2.plot(df_ptf.index, df_ptf['price'], 'b-', label='PTF')
+ax2.set_ylabel('PTF', color='b')
+ax2.set_ylim(0, 5000)
+ax2.tick_params(axis='y', labelcolor='b')
 
-for i, v in ptf_gunluk.iterrows():
-    ax2.text(i, v[0], f'{v[0]:.2f}', ha='center', va='bottom', color=color)
+# Format x-axis to show dates clearly
+ax1.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+fig.autofmt_xdate()  # Rotate dates for better readability
 
-ax1.set_ylim(0, 1400)
-plt.title(f"TR GERÇEKLEŞEN TÜKETİM-PTF - {datetime.strftime(now_day, '%d/%m/%Y')}")
+for date, value in df_ptf['price'].items():
+    ax2.annotate(f'{value:.0f}', (date, value), textcoords="offset points", xytext=(0,5), ha='center', color='b')
+
+for date, value in df_dmd['consumption'].items():
+    ax1.annotate(f'{value/1000:.0f}', (date, value/1000), textcoords="offset points", xytext=(0,5), ha='center', color='r')
+
+# Show the plot
+fig.tight_layout()
+plt.title('Gerçekleşen Tük. (GWh) vs PTF')
 plt.show()
